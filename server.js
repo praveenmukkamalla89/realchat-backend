3e89fbf2129006c14c81041467acd9a2876d9751
Praveen Mukkamalla <praveenmukkamalla89@gmail.com>
2025-01-22 18:10:45 +0530

    chat

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+
+const server = http.createServer(app);
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST"]
+    }
+});
+
+io.on('connection', (socket) => {
+    console.log('A user connected:', socket.id);
+
+    socket.on('send_message', (data) => {
+        io.emit('receive_message', data);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('A user disconnected:', socket.id);
+    });
+});
+
+server.listen(4000, () => {
+    console.log('Server running on port 4000');
+});
+
